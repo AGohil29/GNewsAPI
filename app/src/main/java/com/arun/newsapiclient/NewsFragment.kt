496cac3d0e8c75d5a12cdc93398a8b87e1ac9ecf
ArunGohil <arun.r@hungama.com>
2022-03-10 23:46:52 +0530

    changes

--- app/src/main/java/com/arun/newsapiclient/NewsFragment.kt
+++ app/src/main/java/com/arun/newsapiclient/NewsFragment.kt
@@ -26,10 +26,6 @@ class NewsFragment : Fragment() {
     private lateinit var fragmentNewsBinding: FragmentNewsBinding
     private var country = "us"
     private var page = 1
-    private var isScrolling = false
-    private var isLoading = false
-    private var isLastPage = false
-    private var pages = 0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -68,12 +64,6 @@ class NewsFragment : Fragment() {
                      response.data?.let {
                          Log.i("MYTAG","came here ${it.articles.toList().size}")
                          newsAdapter.differ.submitList(it.articles.toList())
-                         if(it.totalResults%20 == 0) {
-                              pages = it.totalResults / 20
-                         }else{
-                             pages = it.totalResults/20+1
-                         }
-                         isLastPage = page == pages
                      }
                }
                 is com.arun.newsapiclient.data.util.Resource.Error->{
@@ -92,53 +82,20 @@ class NewsFragment : Fragment() {
     }
 
     private fun initRecyclerView() {
-       // newsAdapter = NewsAdapter()
         fragmentNewsBinding.rvNews.apply {
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(activity)
-            addOnScrollListener(this@NewsFragment.onScrollListener)
         }
 
     }
 
     private fun showProgressBar(){
-        isLoading = true
         fragmentNewsBinding.progressBar.visibility = View.VISIBLE
     }
 
     private fun hideProgressBar(){
-        isLoading = false
         fragmentNewsBinding.progressBar.visibility = View.INVISIBLE
     }
-
-   private val onScrollListener = object : RecyclerView.OnScrollListener(){
-       override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-           super.onScrollStateChanged(recyclerView, newState)
-           if(newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
-               isScrolling = true
-           }
-
-       }
-
-       override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-           super.onScrolled(recyclerView, dx, dy)
-           val layoutManager = fragmentNewsBinding.rvNews.layoutManager as LinearLayoutManager
-           val sizeOfTheCurrentList = layoutManager.itemCount
-           val visibleItems = layoutManager.childCount
-           val topPosition = layoutManager.findFirstVisibleItemPosition()
-
-           val hasReachedToEnd = topPosition+visibleItems >= sizeOfTheCurrentList
-           val shouldPaginate = !isLoading && !isLastPage && hasReachedToEnd && isScrolling
-           if(shouldPaginate){
-               page++
-               viewModel.getNewsHeadLines(country,page)
-               isScrolling = false
-
-           }
-
-
-       }
-   }
 
    //search
    private fun setSearchView(){
@@ -184,12 +141,6 @@ class NewsFragment : Fragment() {
                    response.data?.let {
                        Log.i("MYTAG","came here ${it.articles.toList().size}")
                        newsAdapter.differ.submitList(it.articles.toList())
-                       if(it.totalResults%20 == 0) {
-                           pages = it.totalResults / 20
-                       }else{
-                           pages = it.totalResults/20+1
-                       }
-                       isLastPage = page == pages
                    }
                }
                is com.arun.newsapiclient.data.util.Resource.Error->{
