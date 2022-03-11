64087b67df4aa658d5b20be0fbeb8f4d469d3646
ArunGohil <arun.r@hungama.com>
2022-03-11 12:37:13 +0530

    changes

--- app/src/main/java/com/arun/newsapiclient/data/model/Source.kt
+++ app/src/main/java/com/arun/newsapiclient/data/model/Source.kt
@@ -6,6 +6,6 @@ import com.google.gson.annotations.SerializedName
 data class Source(
     @SerializedName("name")
     val name: String,
-    @SerializedName("nurlame")
+    @SerializedName("url")
     val url: String
 )
